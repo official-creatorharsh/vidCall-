8fd80324d3e3b322d1c478cb862fe140b3aa892a
Harsh Singh <79015590+official-creatorharsh@users.noreply.github.com>
2021-05-09 18:06:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {})
 
+/**
 server.listen(3000, () => {
+    console.log("Listening on port 3000...")
+})
+**/
+
+server.listen(https://vidcall-snb.herokuapp.com/, () => {
     console.log("Listening on port 3000...")
 })
 
